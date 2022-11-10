d9127f900d75a771c4c4d5eb231cc0cdfe61f4f4
Tufael Ahmed <tahmed0113@gmail.com>
2022-11-10 12:35:33 -0500

    fixe resoponsive size for mobile

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-/*
-
-
-    <script>
-        let words = "Hello World .";
-        let count = words.split(" ").length;
-        let characters = words.length;
-        let spaces = words.split(" ").length-1;
-
-
-
-
-        //how many of a specific letter or word
-
-       
-       
-
-
-console.log(spaces + " " + count + " " + characters);
-
-</script>
-
-*/
 
 function calculate() {
     let getWord = document.getElementById("word").value;
