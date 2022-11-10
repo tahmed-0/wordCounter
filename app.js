163274538365f2c2bc46864fd67f109676ef2d5f
Tufael Ahmed <tahmed0113@gmail.com>
2022-11-09 22:32:36 -0500

    first

---
+++ app.js
@@ -0,0 +1,23 @@
+/*
+
+
+    <script>
+        let words = "Hello World .";
+        let count = words.split(" ").length;
+        let characters = words.length;
+        let spaces = words.split(" ").length-1;
+
+
+
+
+        //how many of a specific letter or word
+
+       
+       
+
+
+console.log(spaces + " " + count + " " + characters);
+
+</script>
+
+*/
